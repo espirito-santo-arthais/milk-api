8659d7d7c8b257f8ee516146b0d9da62bb955ca9
Arthais Systems <espirito.santo.arthais@outlook.com>
2023-03-03 08:24:28 -0300

    Configurar o comando "node server.js" como script de start da aplicação

--- server.js
+++ server.js
@@ -3,6 +3,13 @@ const cors = require("cors");
 
 const app = express();
 
+// set port, listen for requests
+const PORT = process.env.PORT | 8080;
+
+app.listen(PORT, () => {
+  console.log(`O servidor está escutando na porta ${PORT}.`);
+});
+
 const corsOptions = {
   origin: "http://localhost:8081",
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
@@ -16,12 +23,5 @@ app.use(express.urlencoded({ extended: true }));
 
 // simple router
 app.get("/", (req, res) => {
-    res.json({message : "Bem-vindo a aplicação Milk-API!"})
+  res.json({ message: "Bem-vindo a aplicação Milk-API!" });
 });
-
-// set port, listen for requests
-const PORT = process.env.PORT | 8080;
-
-app.listen(PORT, () => {
-    console.log(`O servidor está escutando na porta ${PORT}.`);
-})
