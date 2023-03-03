5ee148fd9dcc76d3411dc4e5fce180ae09882bca
Arthais Systems <espirito.santo.arthais@outlook.com>
2023-03-03 07:50:54 -0300

    Criar o arquivo de entry point server.js

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+const corsOptions = {
+  origin: "http://localhost:8081",
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
+// parse requests of content-type: application/json
+app.use(express.json());
+
+// parse requests of content-type: application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+// simple router
+app.get("/", (req, res) => {
+    res.json({message : "Bem-vindo a aplicação Milk-API!"})
+});
+
+// set port, listen for requests
+const PORT = process.env.PORT | 8080;
+
+app.listen(PORT, () => {
+    console.log(`O servidor está escutando na porta ${PORT}.`);
+})
